f19c9089cfb58ef00b3164c00fb2086318bb1e56
unknown <antonio.zulim@gmail.com>
2021-02-27 19:34:42 +0100

    Dodano citanje csv datoteka i pretvaranje dobivenih podataka u nizove.

--- js/AutobusPodatci.js
+++ js/AutobusPodatci.js
@@ -1,1 +1,24 @@
+let link = "https://raw.githubusercontent.com/AntonioZulim/autobusni-sustav-podatci/main/vozni-red/vozni_red0.csv";
+
+function ReadCSVFile(url)
+{
+    fetch(url)
+        .then(response => response.text())  // cekamo odgovor pa trazimo tekst
+        .then(data => SerializeData(data)); // odgovor prosljedujemo u drugu funkciju
+}
+
+function SerializeData(data = "")
+{
+    let redovi = data.split(/\r\n|\r|\n/g);
+    let podatci = Array();
+    for(i in redovi)
+    {
+        podatci[i] = redovi[i].split(';');
+    }
+    console.log(podatci);
+}
+
+// Poziva Materialize JS
 M.AutoInit();
+
+ReadCSVFile(link);
